aa27616d37e0bb5684c2a95ee0ce5e21a2c9537a
SutuSebastian <sebiitv@gmail.com>
2024-03-21 15:10:38 +0200

    chore: update demo page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { DarkThemeToggle } from "flowbite-react";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center text-2xl">
-      Flowbite React + Next.js
+    <main className="flex min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
+      <h1 className="text-2xl dark:text-white">Flowbite React + Next.js</h1>
+      <DarkThemeToggle />
     </main>
   );
 }
