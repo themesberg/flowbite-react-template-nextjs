3291c1060c28c60d022bcc70b74f918b686db7c2
SutuSebastian <sebiitv@gmail.com>
2024-04-15 09:47:56 +0300

    chore: cleanup tailwind.config invalid theme extend

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,13 +9,7 @@ const config: Config = {
     flowbite.content(),
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   plugins: [flowbite.plugin()],
 };
