8449f8935c23c4735567201b29917e4e7887a36c
Sutu Sebastian <sebiitv@gmail.com>
2025-07-22 15:16:10 +0300

    up packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { ThemeModeScript } from "flowbite-react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeInit } from "../.flowbite-react/init";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -31,6 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ThemeInit />
         {children}
       </body>
     </html>
